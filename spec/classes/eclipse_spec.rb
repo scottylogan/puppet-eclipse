27e6e3aaa03fcd9caa3e0086ebeb2547285de1e2
Scotty Logan <scotty@catbert.net>
2014-09-01 20:46:02 -0700

    fixed spec file

--- spec/classes/eclipse_spec.rb
+++ spec/classes/eclipse_spec.rb
@@ -4,18 +4,21 @@ require 'spec_helper'
 describe 'eclipse' do
 
   describe 'when not specifying a version' do
-    it { should contain_package('Eclipse-luna-R').with({
+    it { should contain_package('eclipse').with({
       :ensure   => 'installed',
-      :provider => 'compressed_app'
+      :provider => 'compressed_app',
     })}
-    it { should contain_package('Eclipse-luna-R').with_source('http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/luna/R/eclipse-standard-luna-R-macosx-cocoa-x86_64.tar.gz')}
+    it { should contain_package('eclipse').with_source('http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/luna/R/eclipse-standard-luna-R-macosx-cocoa-x86_64.tar.gz')}
   end
 
   describe 'when specifying a specific version' do
     let (:params) {{:version => 'kepler', :release => 'SR2'}}
 
-    it { should contain_package('Eclipse-kepler-SR2')}
-    it { should contain_package('Eclipse-kepler-SR2').with_source('http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-macosx-cocoa-x86_64.tar.gz')}
+    it { should contain_package('eclipse').with({
+      :ensure   => 'installed',
+      :provider => 'compressed_app',
+    })}
+    it { should contain_package('eclipse').with_source('http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-macosx-cocoa-x86_64.tar.gz')}
   end
 
 end
