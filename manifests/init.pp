e04a229bb8240d1ffdf4c32387d0bb2a44c1a92b
Scotty Logan <scotty@catbert.net>
2014-09-01 20:45:46 -0700

    added java dependency, symlink from /Applications/Eclipse.app

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,18 @@
 # install 64 bit eclipse
 class eclipse($version='luna', $release='R') {
 
-  package { "Eclipse-${version}-${release}":
+  include java
+
+  package { 'eclipse':
     ensure   => installed,
     provider => 'compressed_app',
     source   => "http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/${version}/${release}/eclipse-standard-${version}-${release}-macosx-cocoa-x86_64.tar.gz"
+  }
+  
+  file { '/Applications/Eclipse.app':
+    ensure => 'link',
+    target => '/Applications/eclipse/Eclipse.app',
+    require => Package['eclipse']
   }
 
 }
