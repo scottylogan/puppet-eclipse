383f0e0fbf4941fd40c890d0bfd60aca1d676524
Scotty Logan <scotty@catbert.net>
2014-09-01 23:48:36 -0700

    fixing urls

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,21 +6,22 @@ class eclipse($version='luna', $release='R') {
   $dl_url = "http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/${version}/${release}/eclipse-standard-${version}-${release}-macosx-cocoa-x86_64.tar.gz"
 
   exec { 'eclipse download':
-    command => "rm -rf ${tmp_dir} && mkdir -p ${tmp_dir} && curl -o ${tmp_dir}/eclipse.tar.gz ${dl_url}",
+    command => "rm -rf ${tmp_dir} && mkdir -p ${tmp_dir} && curl -o ${tmp_dir}/eclipse.tar.gz '${dl_url}'",
+    cwd     => '/',
     notify  => Exec['eclipse repack'],
   }
 
   exec { 'eclipse repack':
     command     => "tar xzf eclipse.tar.gz && mv eclipse ${pkg_name}.app && tar czf ${pkg_name}.tar.gz ${pkg_name}.app",
     cwd         => $tmp_dir,
-    refreshonly => true,
+#    refreshonly => true,
     notify      => Package['eclipse'],
   }
 
   package { 'eclipse':
     ensure   => installed,
     name     => "eclipse-${version}-${release}",
-    source   => "${tmp_dir}/eclipse-${version}-${release}.tar.gz",
+    source   => "file://${tmp_dir}/eclipse-${version}-${release}.tar.gz",
     alias    => 'eclipse',
     provider => compressed_app,
     notify   => File['/Applications/Eclipse.app'],
