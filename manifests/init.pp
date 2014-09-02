9354dbf01d9461dd573288dfb14963ad05357d0e
Scotty Logan <scotty@catbert.net>
2014-09-01 20:27:39 -0700

    rearranged download URL parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class eclipse($version='luna', $release='R') {
   package { "Eclipse-${version}-${release}":
     ensure   => installed,
     provider => 'compressed_app',
-    source   => "http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/${version}/${release}/eclipse-standard-${version}-${release}-macosx-cocoa-x86_64.tar.gz?r=1"
+    source   => "http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/${version}/${release}/eclipse-standard-${version}-${release}-macosx-cocoa-x86_64.tar.gz"
   }
 
 }
--- spec/classes/eclipse_spec.rb
+++ spec/classes/eclipse_spec.rb
@@ -8,14 +8,14 @@ describe 'eclipse' do
       :ensure   => 'installed',
       :provider => 'compressed_app'
     })}
-    it { should contain_package('Eclipse-luna-R').with_source('http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/R/eclipse-standard-luna-R-macosx-cocoa-x86_64.tar.gz?r=1')}
+    it { should contain_package('Eclipse-luna-R').with_source('http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/luna/R/eclipse-standard-luna-R-macosx-cocoa-x86_64.tar.gz')}
   end
 
   describe 'when specifying a specific version' do
     let (:params) {{:version => 'kepler', :release => 'SR2'}}
 
     it { should contain_package('Eclipse-kepler-SR2')}
-    it { should contain_package('Eclipse-kepler-SR2').with_source('http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-macosx-cocoa-x86_64.tar.gz?r=1')}
+    it { should contain_package('Eclipse-kepler-SR2').with_source('http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-macosx-cocoa-x86_64.tar.gz')}
   end
 
 end
