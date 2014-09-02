3630d55f639f662370b4d77607ad7e2a48c80906
Scotty Logan <scotty@catbert.net>
2014-09-01 23:38:34 -0700

    switched to tarball download, repackage and package install

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,47 @@
 # install 64 bit eclipse
 class eclipse($version='luna', $release='R') {
 
-  file { 'eclipse tarball':
-    ensure  => 'file',
-    path    => "/opt/boxen/eclipse/eclipse-${version}-${release}.tar.gz",
-    source  => "http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/${version}/${release}/eclipse-standard-${version}-${release}-macosx-cocoa-x86_64.tar.gz",
-    require => Package['java'],
-    notify  => Exec['eclipse unpack and rename'],
+  $tmp_dir = '/tmp/eclipse'
+  $pkg_name = "eclipse-${version}-${release}"
+  $dl_url = "http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/${version}/${release}/eclipse-standard-${version}-${release}-macosx-cocoa-x86_64.tar.gz"
+
+  exec { 'eclipse download':
+    command => "rm -rf ${tmp_dir} && mkdir -p ${tmp_dir} && curl -o ${tmp_dir}/eclipse.tar.gz ${dl_url}",
+    notify  => Exec['eclipse repack'],
   }
 
-  exec { 'eclipse unpack and rename':
-    command     => "gtar xf eclipse.tar.gz && mv eclipse eclipse-${version}-${release}",
-    cwd         => '/opt/boxen/eclipse',
+  exec { 'eclipse repack':
+    command     => "tar xzf eclipse.tar.gz && mv eclipse ${pkg_name}.app && tar czf ${pkg_name}.tar.gz ${pkg_name}.app",
+    cwd         => $tmp_dir,
     refreshonly => true,
-    notify      => File['/Applications/Eclipse.app'],
+    notify      => Package['eclipse'],
+  }
+
+  package { 'eclipse':
+    ensure   => installed,
+    name     => "eclipse-${version}-${release}",
+    source   => "${tmp_dir}/eclipse-${version}-${release}.tar.gz",
+    alias    => 'eclipse',
+    provider => compressed_app,
+    notify   => File['/Applications/Eclipse.app'],
   }
 
   file { '/Applications/Eclipse.app':
-    ensure  => 'link',
-    target  => "/opt/boxen/eclipse/eclipse-${version}-${release}/Eclipse.app",
-    notify  => Exec['eclipse jvmcapabilities fixup'],
+    ensure  => link,
+    target  => "/Applications/eclipse-${version}-${release}/Eclipse.app",
+    notify  => Exec['eclipse cleanup'],
+  }
+
+  exec { 'eclipse cleanup':
+    command     => "rm -rf ${tmp_dir}",
+    cwd         => '/',
+    refreshonly => true,
+    notify      => Exec['eclipse jvmcapabilities fixup'],
   }
 
   exec { 'eclipse jvmcapabilities fixup':
     command     => 'for pl in /Library/Java/JavaVirtualMachines/jdk1.7*.jdk/Contents/Info.plist; do plutil -replace JavaVM.JVMCapabilities -json \'["CommandLine","BundledApp","JNI"]\' $pl; done',
+    require     => Package['java'],
     refreshonly => true,
     notify      => Exec['eclipse jdk missing symlink'],
   }
