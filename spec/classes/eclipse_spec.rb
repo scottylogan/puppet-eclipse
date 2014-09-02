632142a0443ff3b4b32e02998c6d46aa37b97070
Scotty Logan <scotty@catbert.net>
2014-09-01 23:07:14 -0700

    updated tests to deal with tarballs rather than packages

--- spec/classes/eclipse_spec.rb
+++ spec/classes/eclipse_spec.rb
@@ -4,21 +4,32 @@ require 'spec_helper'
 describe 'eclipse' do
 
   describe 'when not specifying a version' do
-    it { should contain_package('eclipse').with({
-      :ensure   => 'installed',
-      :provider => 'compressed_app',
+    it { should contain_file('eclipse tarball').with({
+      :ensure => 'file',
+      :path   => "/opt/boxen/eclipse/eclipse-luna-R.tar.gz",
+      :source => 'http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/luna/R/eclipse-standard-luna-R-macosx-cocoa-x86_64.tar.gz',
     })}
-    it { should contain_package('eclipse').with_source('http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/luna/R/eclipse-standard-luna-R-macosx-cocoa-x86_64.tar.gz')}
+
+    it { should contain_file('/Applications/Eclipse.app').with({
+      :ensure => 'link',
+      :target => "/opt/boxen/eclipse/eclipse-luna-R/Eclipse.app",
+    })}
   end
 
   describe 'when specifying a specific version' do
     let (:params) {{:version => 'kepler', :release => 'SR2'}}
 
-    it { should contain_package('eclipse').with({
-      :ensure   => 'installed',
-      :provider => 'compressed_app',
+    it { should contain_file('eclipse tarball').with({
+      :ensure => 'file',
+      :path   => "/opt/boxen/eclipse/eclipse-kepler-SR2.tar.gz",
+      :source => 'http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-macosx-cocoa-x86_64.tar.gz',
     })}
-    it { should contain_package('eclipse').with_source('http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-macosx-cocoa-x86_64.tar.gz')}
+
+    it { should contain_file('/Applications/Eclipse.app').with({
+      :ensure => 'link',
+      :target => "/opt/boxen/eclipse/eclipse-kepler-SR2/Eclipse.app",
+    })}
+
   end
 
 end
