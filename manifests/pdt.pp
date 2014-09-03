6d21e69f88156e3ed14316ef827e946d961aee0d
Scotty Logan <scotty@catbert.net>
2014-09-03 03:39:36 -0700

    added PDT

---
+++ manifests/pdt.pp
@@ -0,0 +1,28 @@
+# install PHP Development Tools
+class eclipse::pdt(
+  $ensure = present,
+  $eclipse_version = '4.4',
+  $feature_version = '3.3.0.201406110111',
+  $feature_group = 'org.eclipse.php.feature.group',
+  $feature_repos = 'http://download.eclipse.org/tools/pdt/updates/3.3,http://download.eclipse.org/releases/luna',
+) {
+
+# also org.eclipse.php.mylyn.feature.group=3.3.0.201406110111
+# also org.eclipse.php.test.feature.group=3.3.0.201406110111
+
+  $p2 = "/Applications/Eclipse.app/Contents/MacOS/eclipse -application org.eclipse.equinox.p2.director -noSplash"
+
+  if $ensure == 'present' {
+    exec { 'eclipse egit_mylyn install':
+      command => "${p2} -repository ${feature_repos} -installIU ${feature_group}/${feature_version}",
+      unless  => "test -d ${feature_path}",
+      creates => $feature_path,
+    }
+  } else {
+    exec { 'eclipse egit_mylyn remove':
+      command => "${p2} -uninstallIU ${feature_group}/${feature_version}",
+      onlyif  => "test -d ${feature_path}",
+    }
+  }
+  
+}
