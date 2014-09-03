67113a6425d6393324b93eb80e38c9f8b6219430
Scotty Logan <scotty@catbert.net>
2014-09-03 03:34:29 -0700

    initial attempt at supporting eclipse plugins / features

---
+++ manifests/force_com_ide.pp
@@ -0,0 +1,25 @@
+# install Force.Com IDE
+class eclipse::force_com_ide(
+  $ensure = present,
+  $eclipse_version = '4.4',
+  $feature_version = '31.0.0.201406301722',
+  $feature_group = 'com.salesforce.ide.feature.feature.group',
+  $feature_repos = 'http://media.developerforce.com/force-ide/eclipse42,http://download.eclipse.org/releases/luna',
+) {
+
+  $p2 = "/Applications/Eclipse.app/Contents/MacOS/eclipse -application org.eclipse.equinox.p2.director -noSplash"
+
+  if $ensure == 'present' {
+    exec { 'eclipse egit_mylyn install':
+      command => "${p2} -repository ${feature_repos} -installIU ${feature_group}/${feature_version}",
+      unless  => "test -d ${feature_path}",
+      creates => $feature_path,
+    }
+  } else {
+    exec { 'eclipse egit_mylyn remove':
+      command => "${p2} -uninstallIU ${feature_group}/${feature_version}",
+      onlyif  => "test -d ${feature_path}",
+    }
+  }
+  
+}
