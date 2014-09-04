7c6c863dad77e4d316ac18228df3966204e8e707
Scotty Logan <swl@stanford.edu>
2014-09-03 18:32:02 -0700

    fixed exec names for PDT

--- manifests/pdt.pp
+++ manifests/pdt.pp
@@ -13,13 +13,13 @@ class eclipse::pdt(
   $p2 = "/Applications/Eclipse.app/Contents/MacOS/eclipse -application org.eclipse.equinox.p2.director -noSplash"
 
   if $ensure == 'present' {
-    exec { 'eclipse egit_mylyn install':
+    exec { 'eclipse PDT install':
       command => "${p2} -repository ${feature_repos} -installIU ${feature_group}/${feature_version}",
       unless  => "test -d ${feature_path}",
       creates => $feature_path,
     }
   } else {
-    exec { 'eclipse egit_mylyn remove':
+    exec { 'eclipse PDT remove':
       command => "${p2} -uninstallIU ${feature_group}/${feature_version}",
       onlyif  => "test -d ${feature_path}",
     }
