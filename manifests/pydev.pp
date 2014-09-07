bb856addc44505e667130e73b6e8f6bdc505a41d
Scotty Logan <scotty@catbert.net>
2014-09-06 17:17:32 -0700

    renamed execs

--- manifests/pydev.pp
+++ manifests/pydev.pp
@@ -13,13 +13,13 @@ class eclipse::pydev(
   $p2 = "/Applications/Eclipse.app/Contents/MacOS/eclipse -application org.eclipse.equinox.p2.director -noSplash"
 
   if $ensure == 'present' {
-    exec { 'eclipse PDT install':
+    exec { 'eclipse PyDev install':
       command => "${p2} -repository ${feature_repos} -installIU ${feature_group}/${feature_version}",
       unless  => "test -d ${feature_path}",
       creates => $feature_path,
     }
   } else {
-    exec { 'eclipse PDT remove':
+    exec { 'eclipse PyDev remove':
       command => "${p2} -uninstallIU ${feature_group}/${feature_version}",
       onlyif  => "test -d ${feature_path}",
     }
